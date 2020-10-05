5d5baa23f0694dd7fae12e0efa4c0d43fc550c61
huoju <virushuo@gmail.com>
2020-10-05 02:42:16 -0400

    use the chrome devtool instead of colly

--- cmd/spider_worker.go
+++ cmd/spider_worker.go
@@ -5,10 +5,11 @@ import (
     "fmt"
     "log"
     "time"
+    "net/url"
 	"path/filepath"
     "encoding/json"
 	"github.com/spf13/viper"
-	"github.com/gocolly/colly/v2"
+	//"github.com/gocolly/colly/v2"
     "github.com/streadway/amqp"
     rcrabbitmq "github.com/virushuo/go-amqp-reconnect/rabbitmq"
 	"git.a.jhuo.ca/huoju/traitement/pkg/rabbitmq"
@@ -30,6 +31,8 @@ var (
     webtoken string
     sleeptime int
     fileStoragePath string
+    chromeRpc string
+    chromeRpcTimeout int
 )
 
 var QueueMessageChannel <-chan amqp.Delivery
@@ -58,6 +61,8 @@ func loadconf() {
     webapiendpoint = viper.GetString("WEBAPI")
     webtoken = viper.GetString("JWT_TOKEN")
     sleeptime= viper.GetInt("SLEEP")
+    chromeRpc = viper.GetString("CHROME_DEBUG_RPC")
+    chromeRpcTimeout = viper.GetInt("CHROME_DEBUG_RPC_TIMEOUT")
 }
 
 func amqpQueueConnect(connectstr string, name string, baseRetryDelay int, maxRetries int, chAmqpErr chan *amqp.Error) (*rabbitmq.Queue){
@@ -80,78 +85,112 @@ func readAmqpErrorChannel(c chan *amqp.Error, amqpQueue *rabbitmq.Queue) {
 }
 
 func processTask(d amqp.Delivery, amqpQueue *rabbitmq.Queue){
-            time.Sleep(time.Duration(sleeptime) * time.Millisecond)
-            atask := &types.Task{}
-            err := json.Unmarshal(d.Body, atask)
-            log.Println(err)
-            log.Printf("Received a message: %s", atask)
-            if atask.Type == "SPIDER"  {
-                ataskmeta := &SpiderTask{}
-                err = json.Unmarshal([]byte(atask.Meta), ataskmeta)
-                log.Printf("Message Meta: %s", ataskmeta )
-                log.Printf("Message Url: %s", ataskmeta.Url )
-                chromedevt := scraping.New("http://127.0.0.1:9222", 10 * time.Second)
-                resulthtml,err := chromedevt.Fetch(ataskmeta.Url)
-                fmt.Println("=============cdp result:")
-                fmt.Println(resulthtml)
-                fmt.Println(err)
+    time.Sleep(time.Duration(sleeptime) * time.Millisecond)
+    atask := &types.Task{}
+    err := json.Unmarshal(d.Body, atask)
+    log.Println(err)
+    log.Printf("Received a message: %s", atask)
+    if atask.Type == "SPIDER"  {
+        ataskmeta := &SpiderTask{}
+        err = json.Unmarshal([]byte(atask.Meta), ataskmeta)
+        log.Printf("Message Meta: %s", ataskmeta )
+        log.Printf("Message Url: %s", ataskmeta.Url )
+        chromedevt := scraping.New(chromeRpc, time.Duration(chromeRpcTimeout) * time.Second)
+        resulthtml, err := chromedevt.Fetch(ataskmeta.Url)
+        if err != nil {
+	        fmt.Println("Request URL:", ataskmeta.Url, "failed with response:", err, "\nError:", err)
+            amqpQueue.Retry(&d)
+        } else {
+            succ := true
+            if ataskmeta.SavePage == true {
+                bucket := &storage.FileBucket{fileStoragePath}
 
-
-				c := colly.NewCollector(
-					colly.UserAgent("Mozilla/5.0 (compatible; traitementBot; http://opentraitement.org)"),
-				)
-                c.OnError(func(r *colly.Response, err error) {
-		            fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
-                    amqpQueue.Retry(&d)
-	            })
-
-				c.OnResponse(func(r *colly.Response) {
-					fmt.Println("response code: ", r.StatusCode)
-                    if r.StatusCode == 200 {
-                        succ := true
-                        if ataskmeta.SavePage == true {
-                            bucket := &storage.FileBucket{fileStoragePath}
-
-                            pagecontent,err := html.FindContent(r.Request.URL.String(), string(r.Body))
-                            fileinfo, err := bucket.Save(pagecontent, atask.ID)
-                            if err != nil{
-                                fmt.Println(err)
-                                succ = false
-                            }else{
-                                fmt.Println("save succ", fileinfo)
-                            }
-                        }
-                        if ataskmeta.GatherLink == true {
-                            urlprefix := fmt.Sprintf("%s://%s", r.Request.URL.Scheme,r.Request.URL.Host)
-                            urlmeta_list := html.FindLink(urlprefix, string(r.Body))
-                            webapi := &storage.WebApi{webapiendpoint,webtoken}
-                            err := webapi.SaveUrls(&urlmeta_list)
-                            if err != nil {
-                                fmt.Println(err)
-                                succ = false
-                            }else {
-                                fmt.Println("submit urls succ")
-                            }
-                        }
-
-                        if succ == false {
-                            //retry
-                        } else {
-                            amqpQueue.Succ(&d)
-                        }
-
-                    }else {
-                        amqpQueue.Retry(&d)
-                    }
-				})
-
-				c.OnRequest(func(r *colly.Request) {
-					fmt.Println("Visiting", r.URL.String())
-				})
-
-				// Start scraping on https://hackerspaces.org
-				c.Visit(ataskmeta.Url)
+                pagecontent,err := html.FindContent(ataskmeta.Url, resulthtml)
+                fileinfo, err := bucket.Save(pagecontent, atask.ID)
+                if err != nil{
+                    fmt.Println(err)
+                    succ = false
+                }else{
+                    fmt.Println("save succ", fileinfo)
+                }
             }
+            if ataskmeta.GatherLink == true {
+                u, err := url.Parse(ataskmeta.Url)
+                urlprefix := fmt.Sprintf("%s://%s", u.Scheme,u.Host)
+                urlmeta_list := html.FindLink(urlprefix, resulthtml)
+                webapi := &storage.WebApi{webapiendpoint,webtoken}
+                err = webapi.SaveUrls(&urlmeta_list)
+                if err != nil {
+                    fmt.Println(err)
+                    succ = false
+                }else {
+                    fmt.Println("submit urls succ")
+                }
+            }
+
+            if succ == false {
+                //retry
+            } else {
+                amqpQueue.Succ(&d)
+            }
+        }
+
+
+		//c := colly.NewCollector(
+		//	colly.UserAgent("Mozilla/5.0 (compatible; traitementBot; http://opentraitement.org)"),
+		//)
+        //c.OnError(func(r *colly.Response, err error) {
+	    //    fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
+        //    amqpQueue.Retry(&d)
+	    //})
+
+		//c.OnResponse(func(r *colly.Response) {
+		//	fmt.Println("response code: ", r.StatusCode)
+        //    if r.StatusCode == 200 {
+        //        succ := true
+        //        if ataskmeta.SavePage == true {
+        //            bucket := &storage.FileBucket{fileStoragePath}
+
+        //            pagecontent,err := html.FindContent(r.Request.URL.String(), string(r.Body))
+        //            fileinfo, err := bucket.Save(pagecontent, atask.ID)
+        //            if err != nil{
+        //                fmt.Println(err)
+        //                succ = false
+        //            }else{
+        //                fmt.Println("save succ", fileinfo)
+        //            }
+        //        }
+        //        if ataskmeta.GatherLink == true {
+        //            urlprefix := fmt.Sprintf("%s://%s", r.Request.URL.Scheme,r.Request.URL.Host)
+        //            urlmeta_list := html.FindLink(urlprefix, string(r.Body))
+        //            webapi := &storage.WebApi{webapiendpoint,webtoken}
+        //            err := webapi.SaveUrls(&urlmeta_list)
+        //            if err != nil {
+        //                fmt.Println(err)
+        //                succ = false
+        //            }else {
+        //                fmt.Println("submit urls succ")
+        //            }
+        //        }
+
+        //        if succ == false {
+        //            //retry
+        //        } else {
+        //            amqpQueue.Succ(&d)
+        //        }
+
+        //    }else {
+        //        amqpQueue.Retry(&d)
+        //    }
+		//})
+
+		//c.OnRequest(func(r *colly.Request) {
+		//	fmt.Println("Visiting", r.URL.String())
+		//})
+
+		// Start scraping on https://hackerspaces.org
+		//c.Visit(ataskmeta.Url)
+    }
 }
 
 func main() {
